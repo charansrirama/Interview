d907c39d4b9d658f73794f2a8cf436032c1189b0
Chaceliang <chaceliang@gmail.com>
2012-03-17 15:30:13 -0400

    InfiniteRecursion example

---
+++ Interview/src/th/example/InfiniteRecursion.java
@@ -0,0 +1,24 @@
+package th.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfiniteRecursion {
+	
+	public String toString() {
+		//return "InfiniteRecursion address: " + this + "\n";
+		return "InfiniteRecursion address: " + super.toString() + "\n";
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List<InfiniteRecursion> v = new ArrayList<InfiniteRecursion>();
+		for(int i = 0; i < 10; i++) {
+			v.add(new InfiniteRecursion());
+		}
+		System.out.println(v);
+	}
+
+}

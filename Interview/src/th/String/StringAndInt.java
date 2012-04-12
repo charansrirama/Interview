e0832f881c97649167bd2b15c0496f12aeb3d1c9
Chaceliang <chaceliang@gmail.com>
2012-04-12 10:50:23 -0400

    '

--- Interview/src/th/String/StringAndInt.java
+++ Interview/src/th/String/StringAndInt.java
@@ -2,76 +2,76 @@ package th.String;
 
 public class StringAndInt {
 
-    public static int atoi(String para) {
-	if (para == null || para.trim().length() == 0)
-	    return 0;
-	final char PLUS = '+';
-	final char MINUS = '-';
+	public static int atoi(String para) {
+		if (para == null || para.trim().length() == 0)
+			return 0;
+		final char PLUS = '+';
+		final char MINUS = '-';
 
-	int ret = 0;
-	String str = para.trim();
-	int len = str.length();
-	boolean negativeSign = false;
+		int ret = 0;
+		String str = para.trim();
+		int len = str.length();
+		boolean negativeSign = false;
 
-	for (int i = 0; i < len; i++) {
-	    char ch = str.charAt(i);
-	    if (i == 0) {
-		if (ch == PLUS) {
-		    negativeSign = false;
-		    continue;
-		} else if (ch == MINUS) {
-		    negativeSign = true;
-		    continue;
+		for (int i = 0; i < len; i++) {
+			char ch = str.charAt(i);
+			if (i == 0) {
+				if (ch == PLUS) {
+					negativeSign = false;
+					continue;
+				} else if (ch == MINUS) {
+					negativeSign = true;
+					continue;
+				}
+			}
+
+			if (!(ch >= '0' && ch <= '9')) {
+				break;
+			} else {
+				ret = 10 * ret + (ch - 48);
+			}
 		}
-	    }
-
-	    if (!(ch >= '0' && ch <= '9')) {
-		break;
-	    } else {
-		ret = 10 * ret + (ch - 48);
-	    }
-	}
-	if (negativeSign)
-	    ret = ret * (-1);
-	return ret;
-    }
-
-    public static String IntToString(int input) {
-	if (input == 0)
-	    return "0";
-	StringBuilder result = new StringBuilder();
-	boolean minus = false;
-
-	int hi = 0;
-
-	if (input < 0) {
-	    minus = true;
-	    input = input * (-1);
+		if (negativeSign)
+			ret = ret * (-1);
+		return ret;
 	}
 
-	while (input != 0) {
-	    hi = input % 10;
-	    input = input / 10;
-	    result.append(hi);
+	public static String IntToString(int input) {
+		if (input == 0)
+			return "0";
+		StringBuilder result = new StringBuilder();
+		boolean minus = false;
+
+		int hi = 0;
+
+		if (input < 0) {
+			minus = true;
+			input = input * (-1);
+		}
+
+		while (input != 0) {
+			hi = input % 10;
+			input = input / 10;
+			result.append(hi);
+		}
+		result.reverse();
+		if (minus)
+			result.insert(0, "-");
+
+		return result.toString();
 	}
-	result.reverse();
-	if (minus)
-	    result.insert(0, "-");
 
-	return result.toString();
-    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(atoi(" 123"));
+		System.out.println(atoi("+123"));
+		System.out.println(atoi("-123"));
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-	System.out.println(atoi(" 123"));
-	System.out.println(atoi("+123"));
-	System.out.println(atoi("-123"));
-
-	System.out.println(IntToString(123));
-	System.out.println(IntToString(-123));
-	System.out.println(IntToString(0));
-    }
+		System.out.println(IntToString(123));
+		System.out.println(IntToString(-123));
+		System.out.println(IntToString(0));
+	}
 
 }

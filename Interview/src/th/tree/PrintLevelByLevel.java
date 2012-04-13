6f01a7115b43c82ef9599b779880873ca08a6cc1
Chaceliang <chaceliang@gmail.com>
2012-04-13 15:20:42 -0400

    Print binary tree level by level

---
+++ Interview/src/th/tree/PrintLevelByLevel.java
@@ -0,0 +1,40 @@
+package th.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+
+public class PrintLevelByLevel {
+
+	public static void printLevelByLevel(Node root) {
+		if(root == null) {
+			System.out.println("null");
+			return;
+		}
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+		
+		while (!queue.isEmpty()) {
+			for (int i = 0; i < queue.size(); i++) {
+				Node node = queue.poll();
+				System.out.print(node.number + " ");
+				if (node.leftChild != null) {
+					queue.add(node.leftChild);
+				}
+				if (node.rightChild != null) {
+					queue.add(node.rightChild);
+				}
+			}
+			System.out.println();
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

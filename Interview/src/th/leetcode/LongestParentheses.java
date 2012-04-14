388d567d1c5f191edf98ce9b34db506b45105a05
Chaceliang <chaceliang@gmail.com>
2012-04-14 15:01:42 -0400

    Accepted. Find longest valid parentheses.

---
+++ Interview/src/th/leetcode/LongestParentheses.java
@@ -0,0 +1,53 @@
+package th.leetcode;
+
+public class LongestParentheses {
+	public static int longestValidParentheses(String s) {
+		int count = 0;
+		int maxLength = 0;
+		int runningLength = 0;
+		
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == '('){
+				count++;
+				runningLength++;
+			} else if(s.charAt(i) == ')'){
+				count--;
+				runningLength++;
+			}
+			if(count == 0 && runningLength > maxLength) {
+				maxLength = runningLength;
+			} else if(count < 0) {
+				runningLength = 0;
+				count = 0;
+			}
+		}
+		
+		count = 0; 
+		runningLength = 0;
+		
+		for(int i = s.length()-1; i >= 0; i--) {
+			if(s.charAt(i) == ')'){
+				count++;
+				runningLength++;
+			} else if(s.charAt(i) == '('){
+				count--;
+				runningLength++;
+			}
+			if(count == 0 && runningLength > maxLength) {
+				maxLength = runningLength;
+			} else if(count < 0) {
+				runningLength = 0;
+				count = 0;
+			}
+		}
+		
+		return maxLength;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(LongestParentheses.longestValidParentheses("(()"));
+	}
+
+}

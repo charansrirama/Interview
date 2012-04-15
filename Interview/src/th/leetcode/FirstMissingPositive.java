41027f1fadd49fba1ffd19be97100fd6ca53ab86
Chaceliang <chaceliang@gmail.com>
2012-04-14 23:59:12 -0400

    Find first missing positive number

---
+++ Interview/src/th/leetcode/FirstMissingPositive.java
@@ -0,0 +1,36 @@
+package th.leetcode;
+
+public class FirstMissingPositive {
+
+	public static int firstMissingPositive(int[] A) {
+		if (A.length == 0)
+			return 1;
+		int max = 0;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] > max)
+				max = A[i];
+		}
+		int[] flag = new int[max + 2];
+		int result = 0;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] > 0)
+				flag[A[i]] = 1;
+		}
+		for (int i = 1; i < flag.length; i++) {
+			if (flag[i] == 0) {
+				result = i;
+				break;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int A[] = new int[] { 1000, -1 };
+		System.out.println(firstMissingPositive(A));
+	}
+
+}

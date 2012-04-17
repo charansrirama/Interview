e164cfb27bca49f6b0516c626b032bc76573ddbd
Chaceliang <chaceliang@gmail.com>
2012-04-17 15:33:42 -0400

    Add matrix printer

--- Interview/src/th/util/Printer.java
+++ Interview/src/th/util/Printer.java
@@ -7,4 +7,14 @@ public class Printer {
 		
 		System.out.println();
 	}
+	
+	public static void matrixPrinter(int[][] matrix) {
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
 }

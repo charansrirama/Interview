fe720599ed513d985c33bf6ab9bfcd68a4b12d39
Chaceliang <chaceliang@gmail.com>
2012-04-18 13:31:24 -0400

    Find Max substree

---
+++ Interview/src/th/tree/FindMaxSubtree.java
@@ -0,0 +1,21 @@
+package th.tree;
+
+public class FindMaxSubtree {
+	public static int maxSum = Integer.MIN_VALUE;
+	public static Node maxTree = null;
+	
+	public static int find(Node node) {
+		if(node == null) {
+			return 0;
+		}
+		
+		int l = find(node.leftChild);
+		int r = find(node.rightChild);
+		int sum = l + r + node.number;
+		if(sum > maxSum) {
+			maxTree = node;
+			maxSum = sum;
+		}
+		return sum;
+	}
+}

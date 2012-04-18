835311a2804811b3b7f6875a7091d7b72b9f1434
Chaceliang <chaceliang@gmail.com>
2012-04-17 23:43:56 -0400

    Add merge sort function.

--- Interview/src/th/util/Sort.java
+++ Interview/src/th/util/Sort.java
@@ -32,4 +32,47 @@ public class Sort {
 		int right = input.length-1;
 		quickSort(input, left, right);
 	}
+	
+	// Merge Sort
+	private static void merge(int[] data, int first, int n1, int n2) {
+		int[] temp = new int[n1+n2];
+		int copied = 0;
+		int copied1 = 0;
+		int copied2 = 0;
+		
+		while((copied1 < n1)&&(copied2 < n2)) {
+			if(data[first+copied1] < data[first+n1+copied2]) {
+				temp[copied++] = data[first+(copied1++)];
+			} else {
+				temp[copied++] = data[first+n1+(copied2++)];
+			}
+		}
+		while(copied1 < n1) {
+			temp[copied++] = data[first+(copied1++)];
+		}
+		while(copied2 < n2) {
+			temp[copied++] = data[first+n1+(copied2++)];
+		}
+		for(int i = 0; i < n1+n2; i++) {
+			data[first+i] = temp[i];
+		}
+	}
+	
+	private static void mergesort(int[] data, int first, int n) {
+		int n1;
+		int n2;
+		if(n > 1) {
+			n1 = n/2;
+			n2 = n-n1;
+			
+			mergesort(data, first, n1);
+			mergesort(data, first+n1, n2);
+			
+			merge(data, first, n1, n2);
+		}
+	}
+	
+	public static void mergesort(int[] data) {
+		mergesort(data, 0, data.length-1);
+	}
 }

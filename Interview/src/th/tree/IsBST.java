586e59116b2ec2cc11442958dd9f09f3524e39bd
Chaceliang <chaceliang@gmail.com>
2012-04-18 21:44:14 -0400

    isBst Function

---
+++ Interview/src/th/tree/IsBST.java
@@ -0,0 +1,28 @@
+package th.tree;
+
+public class IsBST {
+
+	private boolean isBSTUtil(Node node, int min, int max) {
+		if(node == null) {
+			return true;
+		}
+		
+		if(node.number < min || node.number > max) {
+			return false;
+		}
+		
+		return isBSTUtil(node.leftChild, min, node.number-1) && isBSTUtil(node.rightChild, node.number+1, max);
+	}
+	
+	public boolean isBST(Node node) {
+		return isBSTUtil(node, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

0b35c9745c998b51ff57fb2dfca345e3ca93b78d
Chaceliang <chaceliang@gmail.com>
2012-04-28 15:33:26 -0400

    Do change. make it clear

--- Interview/src/th/leetcode/StrStr.java
+++ Interview/src/th/leetcode/StrStr.java
@@ -6,31 +6,46 @@ public class StrStr {
         // Start typing your Java solution below
         // DO NOT write main() function
         if(needle.length() == 0) return haystack;
-        if(needle.length() > haystack.length()) return null;
+//        if(needle.length() > haystack.length()) return null;
         
         int start = 0;
         int running = 0;
         
         boolean find = false;
-        for(int i = 0; i < haystack.length(); i++) {
+//        for(int i = 0; i < haystack.length(); i++) {
+//        	if(haystack.charAt(i) == needle.charAt(0)) {
+//        		find = true;
+//        		start = i;
+//        		running = 1;
+//        		while(running < needle.length()) {
+//        			if((start+running) >= haystack.length()){
+//        				find = false;
+//        				break;
+//        			}
+//        			if(needle.charAt(running) == haystack.charAt(start+running)) {
+//        				running++;
+//        				find = true;
+//        			} else {
+//        				find = false;
+//        				break;
+//        			}
+//        		}
+//        		
+//        		if(find) break;
+//        	}
+//        }
+        for(int i = 0; i <= haystack.length()-needle.length(); i++) {
         	if(haystack.charAt(i) == needle.charAt(0)) {
-        		find = true;
         		start = i;
         		running = 1;
+        		find = true;
         		while(running < needle.length()) {
-        			if((start+running) >= haystack.length()){
+        			if(haystack.charAt(start+running) != needle.charAt(running)) {
         				find = false;
         				break;
         			}
-        			if(needle.charAt(running) == haystack.charAt(start+running)) {
-        				running++;
-        				find = true;
-        			} else {
-        				find = false;
-        				break;
-        			}
+        			running++;
         		}
-        		
         		if(find) break;
         	}
         }

0957cb9b5049523f3411b2a11f6054d65e764508
Chaceliang <chaceliang@gmail.com>
2012-07-31 00:05:26 -0700

    Find num appear more than half times

--- Interview/src/th/c/Arrays/NumAppearMoreThanHalf.java
+++ Interview/src/th/c/Arrays/NumAppearMoreThanHalf.java
@@ -23,4 +23,30 @@ public class NumAppearMoreThanHalf {
 		if(times*2 <= len) return -1;
 		return result;
 	}
+	
+	public int find(int[] num) {
+		int len = num.length;
+		if(len == 0) return -1;
+		int can1 = 0, can2 = 0;
+		int times1 = 0, times2 = 0;
+		for(int i = 0; i < len; i++) {
+			if(times1 == 0) {
+				can1 = num[i];
+				times1 = 1;
+			} else if(times2 == 0) {
+				can2 = num[i];
+				times2 = 1;
+			} else {
+				if(can1 == num[i]) 
+					times1++;
+				else if(can2 == num[i]) 
+					times2++;
+				else {
+					times1--;
+					times2--;
+				}
+			}
+		}
+		return times1>times2?can1:can2;
+	}
 }

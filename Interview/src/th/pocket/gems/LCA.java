f7b5f1f1f1347f5c81f454619b1587405a9f032d
Chaceliang <chaceliang@gmail.com>
2012-08-08 22:15:15 -0700

    LCA

---
+++ Interview/src/th/pocket/gems/LCA.java
@@ -0,0 +1,17 @@
+package th.pocket.gems;
+
+public class LCA {
+	public static int LCA(Node node, int a, int b) {
+		if(node == null) return -999;
+		if(node.val == a || node.val == b) return node.val;
+		int l = LCA(node.left, a, b);
+		int r = LCA(node.right, a, b);
+		if(l == -999 && r == -999) return node.val;
+		return (l == -999) ? r : l;
+	}
+}
+
+class Node {
+	int val;
+	Node left, right;
+}

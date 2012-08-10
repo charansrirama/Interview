d1a2dd7bb3f9e4d9a3940f619e60c3310a722885
Chaceliang <chaceliang@gmail.com>
2012-08-09 23:07:20 -0700

    Write a function that compares two strings and returns a third string containing only the letters that appear in both.

---
+++ Interview/src/th/pocket/gems/FindOverlap.java
@@ -0,0 +1,28 @@
+package th.pocket.gems;
+
+public class FindOverlap {
+
+	public static String findOverlap(String a, String b) {
+		int[] flag = new int[26];
+		StringBuilder result = new StringBuilder();
+		for(int i = 0; i < a.length(); i++) {
+			flag[a.charAt(i) - 'a']++;
+		}
+		
+		for(int i = 0; i < b.length(); i++) {
+			if(flag[b.charAt(i) - 'a'] > 0) {
+				result.append(b.charAt(i));
+				flag[b.charAt(i) - 'a']--;
+			}
+		}
+		return result.toString();
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(findOverlap("abcda", "aba"));
+	}
+
+}

8b59b6076a54ad4516476e2a596ce38b9b543c08
Chaceliang <chaceliang@gmail.com>
2012-08-09 23:44:02 -0700

    18.Write a method which will remove any given character from a String.

---
+++ Interview/src/th/pocket/gems/RemoveCharacter.java
@@ -0,0 +1,27 @@
+package th.pocket.gems;
+
+public class RemoveCharacter {
+
+	public static String removeCharacter(String original, String remove) {
+		int[] flag = new int[52];
+		StringBuilder result = new StringBuilder();
+		for(int i = 0; i < remove.length(); i++) {
+			flag[remove.charAt(i) - 'a']++;
+		}
+		for(int i = 0; i < original.length(); i++) {
+			if(flag[original.charAt(i) - 'a'] == 0) {
+				result.append(original.charAt(i));
+			}
+		}
+		return result.toString();
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Case 1: " + removeCharacter("abcdefghijk", "ajk"));
+		System.out.println("Case 2: " + removeCharacter("aaaaaajljljljljljljl", "ajk"));
+	}
+
+}

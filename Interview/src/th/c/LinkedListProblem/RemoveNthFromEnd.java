7f150cd1a4bae26607a8f02f6b7309e806c5cb98
Chaceliang <chaceliang@gmail.com>
2012-08-11 17:45:52 -0700

    Fix remove head pointer bug.

--- Interview/src/th/c/LinkedListProblem/RemoveNthFromEnd.java
+++ Interview/src/th/c/LinkedListProblem/RemoveNthFromEnd.java
@@ -1,10 +1,15 @@
 package th.c.LinkedListProblem;
 
 public class RemoveNthFromEnd {
-	public Node removeNthFromEnd(Node head, int n) {
+	public static Node removeNthFromEnd(Node head, int n) {
 		Node first = head, second = head;
 		for(int i = 0; i < n; i++) {
 			first = first.next;
+		}
+		if(first == null) {
+			head.val = head.next.val;
+			head.next = head.next.next;
+			return head;
 		}
 		while(first.next != null) {
 			first = first.next;
@@ -12,5 +17,21 @@ public class RemoveNthFromEnd {
 		}
 		second.next = second.next.next;
 		return head;
+	}
+	
+	public static void printList(Node head) {
+		Node n = head;
+		while(n != null) {
+			System.out.print(n.val + " ");
+			n = n.next;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		removeNthFromEnd(head, 3);
+		printList(head);
 	}
 }

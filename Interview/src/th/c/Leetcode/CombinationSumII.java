945b0570e41eb319778fa33d04c93315feccfdf1
Chaceliang <chaceliang@gmail.com>
2012-08-27 22:31:19 -0700

    Combination sum II

---
+++ Interview/src/th/c/Leetcode/CombinationSumII.java
@@ -0,0 +1,45 @@
+package th.c.Leetcode;
+
+import java.util.*;
+
+public class CombinationSumII {
+
+	public static ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
+		Arrays.sort(num);
+		ArrayList<ArrayList<Integer>> results = new ArrayList<ArrayList<Integer>>();
+		HashSet<ArrayList<Integer>> tmp = new HashSet<ArrayList<Integer>>();
+		help(num, new ArrayList<Integer>(), target, 0, 0, tmp);
+		for(ArrayList<Integer> result : tmp) {
+			results.add(result);
+		}
+		return results;
+	}
+	
+	public static void help(int[] num, ArrayList<Integer> list, int target, int sum, int step, HashSet<ArrayList<Integer>> results) {
+		if(sum == target) {
+			ArrayList<Integer> result = new ArrayList<Integer>(list);
+			results.add(result);
+		}
+		if(sum >= target) return;
+		for(int i = step; i < num.length; i++) {
+			list.add(num[i]);
+			help(num, list, target, sum+num[i], i+1, results);
+			list.remove(list.size()-1);
+		}
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int num[] = new int[]{10,1,2,7,6,1,5};
+		ArrayList<ArrayList<Integer>> result = combinationSum2(num, 8);
+		for(ArrayList<Integer> al : result) {
+			for(Integer i : al) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}

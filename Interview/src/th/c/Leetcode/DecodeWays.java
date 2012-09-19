960e2e70ba6d7fdd5655ec01381778d6593d05e3
Chaceliang <chaceliang@gmail.com>
2012-09-18 22:52:07 -0700

    Recursion Decode Ways

--- Interview/src/th/c/Leetcode/DecodeWays.java
+++ Interview/src/th/c/Leetcode/DecodeWays.java
@@ -18,14 +18,14 @@ public class DecodeWays {
 		} else {
 			int v = Character.getNumericValue(s.charAt(0));
 			if(v >= 1 && v <= 9) {
-				re += (char)(v+65);
+				re += (char)(v+64);
 				helper(s.substring(1), re, results);
 			} 
 			if(s.length() >= 2) {
 				int v1 = Character.getNumericValue(s.charAt(0));
 				int v2 = Character.getNumericValue(s.charAt(1));
 				if((v1 == 1 && v2 >= 0 && v2 <= 9)|| (v1 == 2 && v2 >= 0 && v2 <= 6)) {
-					re += (char)(v1*10+v2+65);
+					re += (char)(v1*10+v2+64);
 					helper(s.substring(2), re, results);
 				}
 			}

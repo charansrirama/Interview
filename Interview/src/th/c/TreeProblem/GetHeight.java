2eaa06fbfadf019a179b1477d3111ff1866421d3
Chaceliang <chaceliang@gmail.com>
2012-10-17 21:59:46 -0700

    Add Non-recursion version

--- Interview/src/th/c/TreeProblem/GetHeight.java
+++ Interview/src/th/c/TreeProblem/GetHeight.java
@@ -1,9 +1,28 @@
 package th.c.TreeProblem;
 
+import java.util.*;
+
 public class GetHeight {
 	public int getHeight(Node node) {
 		if(node == null) return 0;
 		else 
 			return Math.max(getHeight(node.left), getHeight(node.right))+1;
+	}
+	
+	// Non-recursive version
+	public static int getHeight2(Node root) {
+		if(root == null) return 0;
+		int height = 0;
+		Queue<Node> q = new LinkedList<Node>();
+		q.offer(root);
+		while(!q.isEmpty()) {
+			height++;
+			for(int i = 0; i < q.size(); i++) {
+				Node t = q.poll();
+				if(t.left != null) q.offer(t.left);
+				if(t.right != null) q.offer(t.right);
+			}
+		}
+		return height;
 	}
 }

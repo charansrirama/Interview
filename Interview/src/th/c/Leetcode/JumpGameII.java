782ae4e5ad6a9d7e98789212e07df55b111c4e26
Chaceliang <chaceliang@gmail.com>
2012-11-04 16:30:35 -0800

    Add dp method

--- Interview/src/th/c/Leetcode/JumpGameII.java
+++ Interview/src/th/c/Leetcode/JumpGameII.java
@@ -2,11 +2,12 @@ package th.c.Leetcode;
 
 public class JumpGameII {
 
+	// Greedy algorithm O(n)
 	public int jump(int A[]) {
-		int k = 0;
-		int s = 0;
-		int j = 0;
-		int i = j+1;
+		int k = 0; // The position from where we could jump the farest so far
+		int s = 0; // steps to jump
+		int j = 0; // the last furtherest position we had checked
+		int i = j+1; // index start to check
 		int n = A.length;
 		while(i < n) {
 			i = j+1;
@@ -21,5 +22,21 @@ public class JumpGameII {
 			s++;
 		}
 		return s;
+	}
+	
+	public int jumpDP(int A[]) {
+		if(A.length <= 1) return 0;
+		int len = A.length;
+		int[] dp = new int[len];
+		dp[len-1] = 0;
+		for(int i = len-2; i >= 0; i--) {
+			int step = len;
+			for(int j = 1; j <= A[i] && (i+j) < len; j++) {
+				if(step > dp[i+j]) 
+					step = dp[i+j];
+			}
+			dp[i] = step+1;
+		}
+		return dp[0];
 	}
 }
